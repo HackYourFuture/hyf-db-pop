d860c8e38b60e9770ba14247d20533e9ae077317
Rob Sutter <rob@redtulipsystems.com>
2017-02-18 11:53:58 +0100

    More instructions.

---
+++ README.md
@@ -0,0 +1,5 @@
+# HackYourFuture Database Populator
+### Quickstart
+Create the `hyf.cnf` file. Run `populate.sh config` for instructions.
+Run `populate.sh {week1|week2}` according to your data needs.
+### NB: This is destructive! It doesn't just truncate the tables, it drops them. ###
--- populate.sh
+++ populate.sh
@@ -2,14 +2,42 @@
 # Copyright (C) 2017 HackYourFuture
 # 
 # Truncates and repopulates sample data for a given week.
+
+# The below config file should NOT be stored in git.
 #
+# All parameters are optional, but if not included must be specified on the
+# command line or the defaults are used where applicable.
+HYF_CONFIG=hyf.cnf
+
 # Check command line arguments for which week to populate.
 case $1 in
   week[1-2])
     SQL_FILE=$1data.sql
     ;;
+  config)
+    echo $""
+    echo $"Create ${HYF_CONFIG} in the working directory with the following format:"
+    echo $""
+    echo $"[client]"
+    echo $"user=\${username}"
+    echo $"password=\${password}"
+    echo $"host=\${host}"
+    echo $"port=\${port}"
+    echo $"database=\${database}"
+    echo $""
+    echo $"Then run $0 {week1|week2} to populate the database."
+    echo $""
+    exit 1  
+    ;;
   *)
-    echo $"Usage: $0 {week1|week2}"
+    echo $""
+    echo $"Usage: $0 {config|week1|week2}"
+    echo $""
+    echo $"Actions:"
+    echo $"  config: display instructions for creating the config file"
+    echo $"  week1: populate the database with week one data"
+    echo $"  week2: populate the database with week two data"
+    echo $""
     exit 1
 esac
 
@@ -19,23 +47,10 @@ if [[ ! -f ${SQL_FILE} ]]; then
   exit 1
 fi
 
-# The below config file should NOT be stored in git.
-#
-# All parameters are optional, but if not included must be specified on the
-# command line or the defaults are used where applicable.
-HYF_CONFIG=hyf.cnf
-
 # Check that the config file exists.
 if [[ ! -f ${HYF_CONFIG} ]]; then
-  echo $"Configuration file ${HYF_CONFIG} not found"
-  echo $"Create ${HYF_CONFIG} and populate it as follows:"
-  echo $""
-  echo $"[client]"
-  echo $"user=\${username}"
-  echo $"password=\${password}"
-  echo $"host=\${host}"
-  echo $"port=\${port}"
-  echo $"database=\${database}"
+  echo $"Configuration file ${HYF_CONFIG} not found."
+  echo $"Run $0 config for instructions."
   echo $""
   exit 1
 fi
